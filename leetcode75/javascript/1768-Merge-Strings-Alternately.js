a3135604887c87cf0400a98d321dd6b5fa58f7d1
Vinícius Watanabe <vinicius_watanabe@hotmail.com>
2024-02-07 11:13:23 -0800

    Solved 1768

---
+++ leetcode75/javascript/1768-Merge-Strings-Alternately.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+var mergeAlternately = function (word1, word2) {
+	let splitWord1 = word1.split('');
+	let splitWord2 = word2.split('');
+
+	let finalWord = '';
+
+	const mixWords = (w1, w2) => {
+		if (w1.length > w2.length) {
+			for (let i = 0; i < w1.length; i++) {
+				if (w2.length - 1 < i) {
+					finalWord += w1[i];
+				} else {
+					finalWord += w1[i];
+					finalWord += w2[i];
+				}
+			}
+		} else {
+			for (let i = 0; i < w2.length; i++) {
+				if (w1.length - 1 < i) {
+					finalWord += w2[i];
+				} else {
+					finalWord += w1[i];
+					finalWord += w2[i];
+				}
+			}
+		}
+	};
+
+	mixWords(splitWord1, splitWord2);
+
+	return finalWord;
+};
